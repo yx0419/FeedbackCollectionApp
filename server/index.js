ecb15693656dc7417bdf95dc11808718302df07b
Yunjae Cho <yunxics@gmail.com>
2020-04-06 18:13:18 -0700

    started working on google OAuth

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy; //import strategy
+
 const app = express();
+
+passport.use(new googleStrategy());
 
 app.get('/', (request, response) => {
     response.send({ framework: 'express' });
